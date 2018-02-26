6d7e8ffd8c1e18f2c5fe93ffc6e3b129f355e965
Dmitry <dmitr.kurbatov@gmail.com>
2018-02-26 21:30:12 +0100

    moment.js dependency added + always 7 days in the top bar

--- src/Slider.js
+++ src/Slider.js
@@ -3,28 +3,44 @@ import React, { Component } from 'react';
 import collectjs from '../node_modules/collect.js';
 
 import TodaysTaskTable from './TodaysTaskTable';
+import NewTaskForm from './NewTaskForm';
+
+import * as moment from 'moment';
 
 class Slider extends Component {
 	constructor(props) {
 		super(props);
 
-		// to initialize it with today's date
-		const collection = collectjs(this.props.tasks);
-		const grouped = collection.groupBy("date").all();
-		const iterable = getIterable(grouped);
-		const todaysDate = getMmDdYyyy(Date.now());
-		const found = iterable.find(obj => {
-			return obj.key === todaysDate;
-		});
-		const index = iterable.indexOf(found);
+		// get now
+		const currentDate = moment();
 
+		// set now for initialization
 		this.state = {
-			currentIndex: index
+			currentDate: dateObjectToString(currentDate.toObject())
 		}
 
-		this.handleControlClick = this.handleControlClick.bind(this);
 		this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
 		this.handleNewTask = this.handleNewTask.bind(this);
+
+		this.handleControlClick = this.handleControlClick.bind(this);
+		this.onClickPrev = this.onClickPrev.bind(this);
+		this.onClickNext = this.onClickNext.bind(this);
+	}
+
+	onClickPrev() {
+		const currentDate = this.state.currentDate;
+		const startOfWeek = moment(currentDate, "MM-DD-YYYY").startOf('week');
+		const prevDay = startOfWeek.day(-1).toObject();
+		const prevDayStr = dateObjectToString(prevDay);
+		this.setState({currentDate: prevDayStr});
+	}
+
+	onClickNext() {
+		const currentDate = this.state.currentDate;
+		const startOfWeek = moment(currentDate, "MM-DD-YYYY").startOf('week');
+		const nextDay = startOfWeek.day(7).toObject();
+		const nextDayStr = dateObjectToString(nextDay);
+		this.setState({currentDate: nextDayStr});
 	}
 
 	handleTimeUpdate(obj) {
@@ -35,8 +51,8 @@ class Slider extends Component {
     this.props.onHandleNewTask(task);
   }
 
-	handleControlClick(index) {
-		this.setState({ currentIndex: index });
+	handleControlClick(date) {
+		this.setState({ currentDate: date });
 	}
 
 	render() {
@@ -46,22 +62,36 @@ class Slider extends Component {
 		const iterable = getIterable(grouped);
 
 		let controls = [];
-		let table;
+		let table = null;
+		let form = null;
 
-		iterable.forEach((object, index) => {
-			const date = object.key;
-			const isOn = this.state.currentIndex === index;
+		const daysOfWeek = getWeekArray(this.state.currentDate);
+
+		daysOfWeek.forEach((date, index) => {
+
+			const isOn = this.state.currentDate === date;
+
 			if(isOn) {
-				table = <TodaysTaskTable
-	        tasks={object.items}
-	        date={date}
-	        onHandleTimeUpdate={this.handleTimeUpdate}
-	        onHandleNewTask={this.handleNewTask} />
+
+				if(grouped[date]) {
+					const tasks = grouped[date].items;
+
+					table = <TodaysTaskTable
+				        tasks={tasks}
+				        onHandleTimeUpdate={this.handleTimeUpdate}
+				        onHandleNewTask={this.handleNewTask} />;
+				}
+
+				form =
+					<NewTaskForm
+						date={date}
+						onHandleNewTask={this.handleNewTask} />
 			}
+
 			controls.push(
 				<Control
-					key={index}
 					isOn={isOn}
+					key={index}
 					date={date}
 					index={index}
 					onHandleControlClick={this.handleControlClick} />
@@ -72,17 +102,20 @@ class Slider extends Component {
 			<div className="Slider">
 				<div className="control btn-toolbar justify-content-center">
 					<div className="btn-group mr-2">
-						<button className="btn btn-sm btn-outline-secondary">{"<"}</button>
+						<button className="btn btn-sm btn-outline-secondary" onClick={this.onClickPrev}>{"<"}</button>
 					</div>
 					<div className="btn-group mr-2">
 						{controls}
 					</div>
 					<div className="btn-group">
-						<button className="btn btn-sm btn-outline-secondary">{">"}</button>
+						<button className="btn btn-sm btn-outline-secondary" onClick={this.onClickNext}>{">"}</button>
 					</div>
 				</div>
 				<div>
 					{table}
+				</div>
+				<div>
+					{form}
 				</div>
 			</div>
 		);
@@ -96,7 +129,7 @@ class Control extends Component {
 	}
 
 	handleControlClick() {
-		this.props.onHandleControlClick(this.props.index);
+		this.props.onHandleControlClick(this.props.date);
 	}
 
 	render() {
@@ -106,9 +139,24 @@ class Control extends Component {
 	}
 }
 
-function getMmDdYyyy(timestamp) {
-  const t = new Date(timestamp);
-  return (t.getMonth() + 1) + "-" + t.getDate() + "-" + t.getFullYear();
+function getWeekArray(date) {
+	const NUMBER_OF_DAYS = 7;
+	const startOfWeek = moment(date, "MM-DD-YYYY").startOf('week');
+
+	let daysOfWeek = [];
+
+	daysOfWeek.push(startOfWeek.toObject());
+
+	for (var i = 1; i < NUMBER_OF_DAYS; i++)
+		daysOfWeek.push(startOfWeek.day(i).toObject());
+
+	return daysOfWeek.map(day => {
+		return dateObjectToString(day);
+	});
+}
+
+function dateObjectToString(obj) {
+	return (obj.months + 1) + "-" + obj.date + "-" + obj.years;
 }
 
 function getIterable(object) {
--- src/TodaysTaskTable.js
+++ src/TodaysTaskTable.js
@@ -2,29 +2,18 @@ import React, { Component } from 'react';
 
 import SearchBar from './SearchBar';
 import TaskTable from './TaskTable';
-import NewTaskForm from './NewTaskForm';
 
 class TodaysTaskTable extends Component {
   constructor(props) {
     super(props);
     this.state = { filterText: '' };
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-    this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
-    this.handleNewTask = this.handleNewTask.bind(this);
   }
 
   handleFilterTextChange(filterText) {
     this.setState({
       filterText: filterText
     });
-  }
-
-  handleTimeUpdate(obj) {
-    this.props.onHandleTimeUpdate(obj);
-  }
-
-  handleNewTask(task) {
-    this.props.onHandleNewTask(task);
   }
 
   render() {
@@ -37,9 +26,6 @@ class TodaysTaskTable extends Component {
           onHandleTimeUpdate={this.handleTimeUpdate}
           tasks={this.props.tasks}
           filterText={this.state.filterText} />
-        <NewTaskForm
-          date={this.props.date}
-          onHandleNewTask={this.handleNewTask} />
       </div>
     );
   }
